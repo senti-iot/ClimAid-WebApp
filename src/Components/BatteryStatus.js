fefde125ec3e1e9f6bc0c260501098bb4970fb39
Henrik Hansen <hh@fsck.dk>
2020-02-25 09:24:27 +0100

    fix styling

--- src/Components/BatteryStatus.js
+++ src/Components/BatteryStatus.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const BatteryStatus = (props) => {
 	return (
-		<div style={{ position: 'relative' }}>
-			<div style={{ position: 'absolute', left: 35, top: 7, fontSize: 12, color: '#000' }}>{props.charge}%</div>
-			<img src="/images/battery.svg" alt={props.charge} style={{ maxWidth: 45, marginTop: 5 }} />
+		<div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+			<div style={{ position: 'absolute', top: 7, fontSize: 12, color: '#000', width: 44, paddingRight: 4 }}>{props.charge}%</div>
+			<img src="/images/battery.svg" alt={props.charge} style={{ width: 45, marginTop: 5 }} />
 		</div>
 		
 	);
