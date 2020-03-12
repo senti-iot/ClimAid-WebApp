1d5efe4b08de7a687b3fedbb2ac97474b92e7ae4
Henrik Hansen <hh@fsck.dk>
2020-03-12 12:55:45 +0100

    update sql

--- src/create.sql
+++ src/create.sql
@@ -4,6 +4,7 @@ CREATE TABLE `buildings` (
   `name` varchar(255) NOT NULL,
   `latlong` varchar(255) NOT NULL,
   `image` varchar(50) DEFAULT NULL,
+  `usage` json DEFAULT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`buildingId`),
@@ -12,18 +13,21 @@ CREATE TABLE `buildings` (
 
 CREATE TABLE `roomdevices` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `roomId` bigint unsigned DEFAULT NULL,
-  `deviceId` bigint unsigned DEFAULT NULL,
+  `uuid` char(36) NOT NULL,
+  `roomUuid` char(36) NOT NULL,
+  `device` char(36) NOT NULL,
+  `position` varchar(20) NOT NULL,
+  `gauges` json NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `rooms` (
   `roomId` bigint unsigned NOT NULL AUTO_INCREMENT,
   `uuid` char(36) DEFAULT NULL,
   `buildingId` bigint unsigned DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `bounds` text,
+  `bounds` json DEFAULT NULL,
   `image` varchar(50) DEFAULT NULL,
   `created` datetime DEFAULT CURRENT_TIMESTAMP,
   `modified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
