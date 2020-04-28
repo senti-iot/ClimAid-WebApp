36ca13d1a2a63189315486f92b71e445f46c98a2
Henrik Hansen <hh@fsck.dk>
2020-04-28 15:22:58 +0200

    scroll on table

--- src/Components/Building/BuildingInfoRooms.js
+++ src/Components/Building/BuildingInfoRooms.js
@@ -4,6 +4,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableContainer from '@material-ui/core/TableContainer';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 import buildingStyles from 'Styles/buildingStyles';
@@ -69,30 +70,32 @@ const BuildingInfoRooms = (props) => {
 			{props.rooms.length ?
 				<>
 					<p><b>Lokale</b></p>
-					<Table className={classes.table} aria-label="Lokale tabel">
-						<TableHead>
-							<TableRow>
-								<TableCell align="center">On/Off</TableCell>
-								<TableCell></TableCell>
-								<TableCell align="center">Indeklima</TableCell>
-								<TableCell align="center">Batteriniveau</TableCell>
-								<TableCell></TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{props.rooms.map((room) => {
-								return (
-									<TableRow key={room.uuid} style={{ height: 40, cursor: 'pointer' }} hover onClick={() => props.handleRoomClick(room)}>
-										<TableCell align="center">{getOnlineColorState(room.uuid)}</TableCell>
-										<TableCell>{room.name}</TableCell>
-										<TableCell align="center">{colorStates[room.uuid] ? <BuildingInfoRoomDot color={colorStates[room.uuid]} /> : ''}</TableCell>
-										<TableCell align="center">{batteryStates[room.uuid] ? <BatteryStatus charge={batteryStates[room.uuid]} /> : ''}</TableCell>
-										<TableCell>{/* <Notifications style={{ color: '#ccc' }} /> */}</TableCell>
-									</TableRow>
-								)
-							})}
-						</TableBody>
-					</Table>
+					<TableContainer style={{ maxHeight: 400 }}>
+						<Table stickyHeader className={classes.table} aria-label="Lokale tabel">
+							<TableHead>
+								<TableRow>
+									<TableCell align="center">On/Off</TableCell>
+									<TableCell></TableCell>
+									<TableCell align="center">Indeklima</TableCell>
+									<TableCell align="center">Batteriniveau</TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{props.rooms.map((room) => {
+									return (
+										<TableRow key={room.uuid} style={{ height: 40, cursor: 'pointer' }} hover onClick={() => props.handleRoomClick(room)}>
+											<TableCell align="center">{getOnlineColorState(room.uuid)}</TableCell>
+											<TableCell>{room.name}</TableCell>
+											<TableCell align="center">{colorStates[room.uuid] ? <BuildingInfoRoomDot color={colorStates[room.uuid]} /> : ''}</TableCell>
+											<TableCell align="center">{batteryStates[room.uuid] ? <BatteryStatus charge={batteryStates[room.uuid]} /> : ''}</TableCell>
+											<TableCell>{/* <Notifications style={{ color: '#ccc' }} /> */}</TableCell>
+										</TableRow>
+									)
+								})}
+							</TableBody>
+						</Table>
+					</TableContainer>
 				</>
 				: ""}
 		</>
